aee40e3d782c405b7107362f87e28afa6f2b6ec4
zchelalo <eduardosaavedra687@gmail.com>
2024-08-01 00:15:07 -0700

    update: error managment correctly

--- example/course/main.go
+++ example/course/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	courseTransport := courseSdk.NewHTTPClient("http://localhost:8002", "")
 
-	course, err := courseTransport.Get("9c25d2b1-0865-49b8-af2d-3ef840b5f64c")
+	course, err := courseTransport.Get("9c25d2b1-0865-49b8-af2d-3ef840b5f64ca")
 	if err != nil {
 		if errors.As(err, &courseSdk.ErrNotFound{}) {
 			log.Fatal("course not found: ", err.Error())
